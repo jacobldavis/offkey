474913b42b668b9cae9e7fc8275f4af68a4c3286
Quantiset <ryon.peddapalli@gmail.com>
2026-02-07 10:01:16 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,24 @@
 from scipy.io import wavfile
 from scipy.fft import fft, fftfreq
 import numpy as np
+import matplotlib.pyplot as plt
 
 sample_rate, audio_data = wavfile.read('ex.wav')
-wavfile.write('labubu.wav', sample_rate, audio_data)
+# wavfile.write('labubu.wav', sample_rate, audio_data)
+
+audio_data = audio_data[:, 0]
 
 print(f"Sample rate: {sample_rate} Hz")
 print(f"Audio data shape: {audio_data.shape}")
 
-transform = fft(audio_data)
-xf = fftfreq(len(audio_data), 1 / sample_rate)
+N = len(audio_data)
 
-print(transform.shape)
-print(xf.shape)
+fft_vals = fft(audio_data)
+fft_magnitude = np.abs(fft_vals[:N // 2])
+freqs = fftfreq(N, 1 / sample_rate)[:N // 2]
+
+plt.plot(freqs, fft_magnitude)
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Amplitude')
+plt.title('Frequency Spectrum')
+plt.show()
